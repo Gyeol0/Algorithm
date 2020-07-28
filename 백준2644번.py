882a9c5126c01f463970e2576363c676c61e0132
HangyeolKim <50413349+KimHanGyeol0@users.noreply.github.com>
2020-07-28 18:21:15 +0900

    백준알고리즘

---
+++ 백준2644번.py
@@ -0,0 +1,37 @@
+from collections import deque
+n=int(input())
+a,b=map(int,input().split())
+m=int(input())
+graph={}
+for i in range(m): #graph 생성(양방향 간선)
+    x,y=map(int,input().split())
+    if x not in graph:
+        graph[x]=[y]
+    else:
+        graph[x].append(y)
+    if y not in graph:
+        graph[y]=[x]
+    else:
+        graph[y].append(x)
+queue=deque()
+visit=[]
+queue.append(a)
+answer=0
+end=0
+while queue:
+    l=len(queue)
+    for i in range(l):
+        k=queue.popleft()
+        if k==b:
+            end=1
+            break
+        if k not in visit:
+            visit.append(k)
+            queue.extend(graph[k])
+    if end==1:
+        break
+    if len(queue)==0:
+        answer=-1
+        break
+    answer+=1
+print(answer)
