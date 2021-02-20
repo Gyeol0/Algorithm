374cce4cc0b87843924297117ef64132b3ef77f3
gyeol <gksruf1128@naver.com>
2021-02-20 23:20:07 +0900

    백준알고리즘

--- 백준7453번.py
+++ 백준7453번.py
@@ -1,32 +1,29 @@
+def Sum_Zero(A, B, C, D):
+    # (A+B)에 -(C+D)가 있는지 확인
+    # 이분탐색으로 찾아야할 것 같은데 이렇게도 풀린다.
+    # 뭔가 이상한 문제 같다
+    AB = {}
+    count = 0
+    # A + B 생성
+    for i in A:
+        for j in B:
+            AB[i+j] = AB.get(i+j, 0) + 1
+
+    # A + B에 -(C + D)가 있는지 확인
+    for i in C:
+        for j in D:
+            count += AB.get(-(i+j),0)
+    return count
+
 N=int(input())
 A=[]
 B=[]
 C=[]
 D=[]
 for _ in range(N):
-    a,b,c,d=map(int,input().split())
+    a, b, c, d=map(int,input().split())
     A.append(a)
     B.append(b)
     C.append(c)
     D.append(d)
-AB={}
-CD={}      
-for i in A:
-    for j in B:
-        if i+j not in AB: #get 사용해도 가능
-            AB[i+j]=1
-        else:
-            AB[i+j]+=1
-for i in C:
-    for j in D:
-        if i+j not in CD:
-            CD[i+j]=1
-        else:
-            CD[i+j]+=1
-
-answer=0
-for i in AB.keys(): #두 집합으로 묶어 절댓값이 같도록 하는 순서쌍
-    if -i in CD.keys():
-        answer+=AB[i]*CD[-i]
-        
-print(answer)
+print(Sum_Zero(A, B, C, D))
